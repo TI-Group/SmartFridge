54bb6dadc0b56c7dba17d31110326e10f00d80fd
IrvingW <thor.wang@sjtu.edu.cn>
2018-06-11 12:35:23 +0800

    upload image

--- app/src/main/java/com/example/irving/smartfridge/MainActivity.java
+++ app/src/main/java/com/example/irving/smartfridge/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.irving.smartfridge;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.media.ImageReader;
@@ -11,13 +12,25 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.PeripheralManager;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Skeleton of an Android Things activity.
@@ -39,6 +52,9 @@ import java.nio.ByteBuffer;
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  */
 public class MainActivity extends Activity{
+    // some contant
+    private static final String UPLOAD_IMAGE_URL = "http://120.78.218.52:8080/fridge/fridgeAction/openFridge";
+    private static final String FRIDGE_ID = "";
     private ImageView imageView;
     private MyCamera mCamera;
     private Handler mCameraHandler;     // for running camera in background
@@ -54,9 +70,11 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.photo);
-        if(checkSelfPermission(Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED){
-            Log.w(TAG, "No Camera permission");
+        // check permission
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            Log.e(TAG, "permission problem");
             finish();
         }
         try{
@@ -98,6 +116,8 @@ public class MainActivity extends Activity{
                 @Override
                 public void run() {
                     imageView.setImageBitmap(BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.length));
+                    File file = convertByteToFile(imageBytes);
+                    uploadImg(file);
                 }
             });
         }
@@ -157,6 +177,69 @@ public class MainActivity extends Activity{
             @Override
             public void onGpioError(Gpio gpio, int error) {
                 Log.w(TAG, mGpio + ": Error event " + error );
+            }
+        });
+    }
+
+    private File convertByteToFile(byte[] imageByte) {
+        File f = null;
+        try {
+            // create a file to write bitmap data
+            f = new File(MainActivity.this.getCacheDir(), "photo");
+            if(f.exists())
+                f.delete();
+            f.createNewFile();
+
+            // write the bytes in file
+            FileOutputStream fos = new FileOutputStream(f);
+            fos.write(imageByte);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            Log.e(TAG, "error when create file");
+        }
+        return f;
+    }
+
+    private void uploadImg(File file){
+        if(file == null){
+            Log.w(TAG, "uploadImg: File is null");
+            return;
+        }
+        OkHttpClient client = new OkHttpClient();
+        MultipartBody.Builder builder = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("fridgeId", FRIDGE_ID)
+                .addFormDataPart("uploadImage", "photo.jpg"
+                    , RequestBody.create(MediaType.parse("image/jpg"), file));
+        RequestBody requestBody = builder.build();
+        Request request = new Request.Builder()
+                .url(UPLOAD_IMAGE_URL)
+                .post(requestBody)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "uploadImg onFailure: "+e );
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "成功"+response);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "成功", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
